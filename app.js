4e7dcd4f99077f44b8e7388fb277ac51b6d2455e
InzamamRahaman <izzyrahaman@gmail.com>
2016-01-28 23:37:14 -0400

    Made changes to navbar

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 //var path = __dirname + '/views/';
-var path = require('path')
+var path = require('path');
 app.use(express.static(path.join(__dirname, 'views')));
+
+
 app.get('/', function(req, res) {
-   res.sendFile(path + '/index.html');
+   res.sendFile('/index.html');
+});
+
+app.get('/about', function(req, res) {
+   res.sendFile('/about.html')
+});
+
+app.get('/contact', function(req, res) {
+   res.sendFile('/contact');
+});
+
+app.get('/research', function(req, res) {
+   console.log('Research file to be completed');
+});
+
+app.get('/members', function(req, res) {
+   console.log('Members to be added');
 });
 
 app.listen(3000, function() {
