17ea9e11d3bc06f82fe9b9e12a3fe851137c84db
InzamamRahaman <izzyrahaman@gmail.com>
2016-01-28 23:29:52 -0400

    Fixed issue with css

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-var path = __dirname;
-
-
+//var path = __dirname + '/views/';
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'views')));
 app.get('/', function(req, res) {
    res.sendFile(path + '/index.html');
 });
