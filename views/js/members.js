3b9b065cc9fbc7286e6ade4ff22b71584a30a054
steffanboodhoo <boodhoo100@gmail.com>
2016-02-09 00:53:40 -0400

    css fixes for images

--- views/js/members.js
+++ views/js/members.js
@@ -43,7 +43,7 @@
 			// img = "http://localhost:3000/default-profile.png";
 			if(data[name]['img'])
 				img = window.location.href.replace(window.location.pathname,"") + "/" + data[name]['img'];
-			left.append( $("<div/>",{"class":"circular"}).css('background','url('+img+') ') )
+			left.append( $("<div/>",{"class":"circular"}).css('background','url('+img+') no-repeat') )
 			
 			//RIGHT
 			//right_sub_left
