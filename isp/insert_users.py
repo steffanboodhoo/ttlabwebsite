27ef1fcf228e6c54d80510c39adc70344df93a7f
InzamamRahaman <izzyrahaman@gmail.com>
2016-02-09 16:16:39 -0400

    Added changes

--- isp/insert_users.py
+++ isp/insert_users.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+
+
 import sqlite3
 import sys
 
 
 connection_string = 'data.db'
 
-input_data = sys.stdin.read()
-email_addrs = input_data.strip().split()
-email_addrs = map(lambda s: s.upper(), email_addrs)
+input_data = sys.argv[1 : ]
+email_addrs = map(lambda s: s.upper(), input_data)
+
+print email_addrs
 
 base_stmnt = "INSERT INTO SENDERS VALUES('{0}');"
 
@@ -15,3 +19,4 @@ conn = sqlite3.connect(connection_string)
 for email_addr in email_addrs:
     stmnt = base_stmnt.format(email_addr)
     conn.execute(stmnt)
+    conn.commit()
