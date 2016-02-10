8246b88ee47c3dedfc742510941b9679fb17cf20
steffanboodhoo <boodhoo100@gmail.com>
2016-02-10 18:40:22 -0400

    redesign of publications according to data

--- views/js/research.js
+++ views/js/research.js
@@ -21,19 +21,51 @@
 	}
 
 	/*
-		title{
-			description:...,
-			time:...
-		}
+	"A Consumer Focused Open Data Platform":{
+		"author":["C. Millette", "P. Hosein"],
+		"venue":"International Conference on Big Data and Smart City",
+		"city":"Muscat",
+		"country":"Oman",
+		"volume":"",
+		"month":"Mar",
+		"year":"2016",
+		"pdf":"icbdsc2016.pdf"
+	},
 	*/
 	function createList(data){
 		var list = $("<ul/>",{"class":"list"});
+		var pdfs = {}, count = 0;
 		for (var title in data){
+			//save pdf for later
+			pdfs["pdf"+count] = data[title]['pdf'];
+
+			//create containers
 			var li = $("<li/>",{"class":"post-preview "});
-			li.append($("<h3/>",{"class":"post-title list-title"}).append(title));
-			li.append($("<p/>",{"class":"post-subtitle list-description"}).append(data[title]['description']));
-			li.append($("<p/>",{"class":"post-meta list-date"}).append(data[title]['time']));
+			var a = $('<a/>',{"id":"pdf"+count});
+			
+			//add shit
+			a.append( $("<h4/>",{"class":"post-title list-title"}).append(title) );
+			a.append( $("<p/>",{"class":"post-subtitle list-description "}).append(data[title]['authors']) )
+			a.append($("<p/>",{"class":"post-subtitle list-description"}).append(data[title]['location']));
+			li.append(a);
+			var meta = data[title]['date'];
+			if(data[title]["volume"] != "")
+				meta += ", vol"+data[title]["volume"];
+			li.append($("<p/>",{"class":"post-meta list-date"}).append(meta));
+			
 			list.append(li);
+
+			//onclick event
+			a.click(function(event){
+				var id = event.target.id;
+				if(id == "")
+					id = $(event.target).parent().attr('id');
+				var pdf = "#";
+				if( pdfs[id] != "")
+					pdf = pdfs[id];
+				window.location = "/pdf/"+ pdf;	
+			})
+			count++;
 		}	
 
 		list.appendTo('#list_cont');
